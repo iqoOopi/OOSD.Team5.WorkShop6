df08567d0acef132b0524e6ea2aa463a6b1d4bac
EricT135 <eric.truong.135@gmail.com>
2019-06-06 09:08:07 -0600

    create login page

--- src/application/Main.java
+++ src/application/Main.java
@@ -25,9 +25,9 @@ public class Main extends Application {
         PackageController.passStage(primaryStage);
         Controller.passStage(primaryStage);
 
-        Parent root = FXMLLoader.load(getClass().getResource("../view/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../view/authentication.fxml"));
         primaryStage.setTitle("Travel Experts Database Management");
-        primaryStage.setScene(new Scene(root, 1200, 650));
+        primaryStage.setScene(new Scene(root, 806, 489));
         primaryStage.show();
     }
 
